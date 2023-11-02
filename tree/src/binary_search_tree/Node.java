1e9ddb270f7122f02c6acc6af03101a26241b7b3
shubham <shubhamerande2@gmail.com>
2023-11-02 17:32:41 +0530

    tree comit

---
+++ tree/src/binary_search_tree/Node.java
@@ -0,0 +1,38 @@
+package binary_search_tree;
+
+public class Node {
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public Node getRight() {
+		return right;
+	}
+
+	public void setRight(Node right) {
+		this.right = right;
+	}
+
+	public Node getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node left) {
+		this.left = left;
+	}
+
+	private int value;
+	private Node right;
+	private Node left;
+
+	public Node(int value) {
+		this.value = value;
+		right = null;
+		left = null;
+	}
+
+}
